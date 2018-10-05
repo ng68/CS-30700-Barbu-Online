2c4b1544b7680328ac8df1ad3d5d473d77876c16
Edhballa7 <compton0@purdue.edu>
2018-10-04 20:38:21 -0400

    sighnup

--- Signup/Signup.js
+++ Signup/Signup.js
@@ -24,11 +24,7 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
             //Initializing the user info in Firebase databasea
             var uid = firebaseUser.uid;
             window.alert(uid);
-            firebase.database().ref("Users/" + uid).set({
-                Wins: 0,
-                Losses: 0,
-                "Avg Score": 0
-            });
+            firebase.database().ref("Users/"+uid).set("Hello");
 
             firebase.auth().signOut();
             localStorage.clear();
