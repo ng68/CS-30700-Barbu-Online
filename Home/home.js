214bd8249958098e45371583486c76f807f82208
Edhballa7 <compton0@purdue.edu>
2018-10-04 21:58:25 -0400

    Implementing friends list with database

---
+++ Home/home.js
@@ -0,0 +1,8 @@
+var addFriend = document.getElementById("addFriend");
+var removeFriend = document.getElementById("removeFriend");
+var addFriendBtn = document.getElementById("addFriendBtn");
+var removeFriendBtn = document.getElementById("removeFriendBtn");
+
+addFriendBtn.addEventListener('click', e=> {
+    
+})
