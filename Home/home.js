7246ff6727551e1fe60d9f5f2293baf45fe33c46
Edhballa7 <compton0@purdue.edu>
2018-10-04 22:52:50 -0400

    Testing home.js

--- Home/home.js
+++ Home/home.js
@@ -4,9 +4,11 @@ var addFriendBtn = document.getElementById("addFriendBtn");
 var removeFriendBtn = document.getElementById("removeFriendBtn");
 
 addFriendBtn.addEventListener('click', e=> {
+    window.alert("Hello!");
     var query = firebase.database().ref("users").orderByKey();
     query.once("value")
         .then(function(snapshot) {
+            window.alert(snapshot.val());
             snapshot.foreach(function(childSnapshot) {
                 var tempEmail = childSnapshot.child("email");
                 window.alert(tempEmail);
