373f3b1e53d84b073efcfec785ba57831b8b660c
ng68 <ng68@purdue.edu>
2018-10-05 01:02:20 -0400

    Updated rankings stuff

--- Rankings/rankings.js
+++ Rankings/rankings.js
@@ -1,22 +1,22 @@
-function generate_table() {
-	str = ''
-	users = []
-	firebase.database().ref("users").orderByChild("avg score").on("value", function(snapshot) {
-		users.push([snapshot.val().username, snapshot.val().wins, snapshot.val()."avg score");
-	    console.log(snapshot.val().username, snapshot.val()."avg score");
-	});
+console.log("HI");
+str = ''
+users = []
+firebase.database().ref("users").orderByChild("avg_score").on("value", function(snapshot) {
+	users.push([snapshot.val().username, snapshot.val().wins, snapshot.val().avg_score]);
+	console.log(snapshot.val().username, snapshot.val().avg_score);
+});
 
-	users.sort(sort_function)
-	top100 = users.slice(0, 100)
-	for(var i = 0; i < top100.length; i++) {
-		str += '<tr>\n<th scope=\"row\">' + i + '</th>\n';
-		str += '<td>' + top100[i][0] + '</td>\n';
-		str += '<td>' + top100[i][1] + '</td>\n';
-		str += '<td>' + top100[i][2] + '</td>\n';
-		str += '</tr>\n';
-	}
-	document.getElementById("table").innerHTML += str;
+users.sort(sort_function)
+top100 = users.slice(0, 100)
+for(var i = 0; i < top100.length; i++) {
+	str += '<tr>\n<th scope=\"row\">' + i + '</th>\n';
+	str += '<td>' + top100[i][0] + '</td>\n';
+	str += '<td>' + top100[i][1] + '</td>\n';
+	str += '<td>' + top100[i][2] + '</td>\n';
+	str += '</tr>\n';
 }
+document.getElementById("table").innerHTML += str;
+
 
 function sort_function(a, b) {
     if(a[2] == b[2]) {
