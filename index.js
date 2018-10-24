3b662056be36ae278fea014b660598a8534af576
Noah Johnson <john1900@purdue.edu>
2018-10-24 14:32:50 -0400

    Added functionality to the server permitting automatic lobby on the lobbies page, with example in test.html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const io = require('socket.io')(8080);
 
 const chatNamespace = io.of('/chat');
-const lobbiesNamespace = io.of('lobbies');
+const lobbiesNamespace = io.of('/lobbies');
+const gamesNamespace = io.of('/games');
 
 let lobbies = [];
 
@@ -13,9 +14,28 @@ chatNamespace.on('connection', socket => {
     console.log("Chat connected");
 });
 
+gamesNamespace.on('connection', socket => {
+    console.log("Games connected");
+});
+
 lobbiesNamespace.on('connection', socket => {
     console.log("Lobbies connected");
 
+    // When the page loads (i.e. once a client joins lobbiesNamespace), want
+    // to send them all of the currently open lobbies
+    lobbies.forEach(lobby => {
+        socket.emit('new-lobby', {
+            owner: lobby.owner,
+            name: lobby.name,
+            players: lobby.players
+        });
+    });
+
+    // Event handler for user creating a new lobby
+    // Expects an object lobbyData of the form {
+    //    owner: String
+    //    name: String
+    // }
     socket.on('lobby-created', lobbyData => {
         let lobbyExists = false;
         lobbies.forEach(lobby => {
@@ -33,12 +53,13 @@ lobbiesNamespace.on('connection', socket => {
             };
 
             lobby.players.push(lobbyData.owner); // Owner should be in lobby
-            lobbies.push(lobby);
-            socket.emit('lobby-created-response', "OK");
+            lobbies.push(lobby); // Add lobby data to global array
+            lobbiesNamespace.emit('new-lobby', lobby); // Tell all clients that a new lobby is made
+            socket.emit('lobby-created-response', "OK"); // Send response to user
         } else {
             // Cannot create lobby either because the owner already has a lobby or
             // the lobby name already exists.
-            socket.emit('lobby-created-response', "ERROR");
+            socket.emit('lobby-created-response', "ERROR"); // Send response to user
         }
     });
 });
