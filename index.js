eef9c28c17090f41f6ad2e6e0e86c972c2d6846c
Noah Johnson <john1900@purdue.edu>
2018-10-24 13:42:37 -0400

    Added the basic code for lobby creation on the backend

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const io = require('socket.io')(8080);
 const chatNamespace = io.of('/chat');
 const lobbiesNamespace = io.of('lobbies');
 
+let lobbies = [];
+
 io.on('connection', socket => {
     console.log("Root connected");
 });
@@ -14,5 +16,29 @@ chatNamespace.on('connection', socket => {
 lobbiesNamespace.on('connection', socket => {
     console.log("Lobbies connected");
 
-    lobbiesNamespace.emit('lobby', "hey");
+    socket.on('lobby-created', lobbyData => {
+        let lobbyExists = false;
+        lobbies.forEach(lobby => {
+            if ((lobby.owner == lobbyData.owner) || (lobby.name == lobbyData.name)) {
+                // Cannot create lobby
+                lobbyExists = true;
+            }
+        });
+
+        if (!lobbyExists) {
+            let lobby = {
+                owner: lobbyData.owner,
+                name: lobbyData.name,
+                players: []
+            };
+
+            lobby.players.push(lobbyData.owner); // Owner should be in lobby
+            lobbies.push(lobby);
+            socket.emit('lobby-created-response', "OK");
+        } else {
+            // Cannot create lobby either because the owner already has a lobby or
+            // the lobby name already exists.
+            socket.emit('lobby-created-response', "ERROR");
+        }
+    });
 });
